1c09a100ec4b436e9a8336ddd894282e3df4b6d8
Jeroen Roovers <jroovers93@gmail.com>
2019-02-18 12:30:19 +0100

    git cucumber maven setup

---
+++ Kwetter/Backend/src/test/java/kwetter/backend/Stepdefs.java
@@ -0,0 +1,43 @@
+package kwetter.backend;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import static org.junit.Assert.*;
+
+class IsItFriday {
+
+    static String isItFriday(String today) {
+        return "Nope";
+    }
+}
+
+public class Stepdefs {
+
+    private String today;
+    private String actualAnswer;
+
+    @Given("^today is Sunday$")
+    public void today_is_Sunday() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        today = "Sunday";
+    }
+
+    @Given("^today is Friday$")
+    public void today_is_Friday() {
+        this.today = "Friday";
+    }
+
+    @When("^I ask whether it's Friday yet$")
+    public void i_ask_whether_it_s_Friday_yet() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("^I should be told \"([^\"]*)\"$")
+    public void i_should_be_told(String expectedAnswer) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
