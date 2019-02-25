975df66b6dfd5b3438658745258c0175a8d779d1
Jeroen Roovers <jroovers93@gmail.com>
2019-02-25 09:30:23 +0100

    Role model

--- Kwetter/Backend/src/main/java/model/Role.java
+++ Kwetter/Backend/src/main/java/model/Role.java
@@ -1,14 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 /**
  *
- * @author jeroe
+ * @author Jeroen
  */
 public class Role {
-    
+
+    private int id;
+    private boolean allowedToViewUsers;
+    private boolean allowedToDeleteKweets;
+    private boolean allowedToManageRoles;
+    private boolean allowedToAssignRoles;
+
+    private Role() {
+
+    }
+
+    private Role(boolean viewUsers, boolean deleteKweets, boolean manageRoles, boolean assignRoles) {
+        this.allowedToViewUsers = viewUsers;
+        this.allowedToDeleteKweets = deleteKweets;
+        this.allowedToManageRoles = manageRoles;
+        this.allowedToAssignRoles = assignRoles;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isAllowedToViewUsers() {
+        return allowedToViewUsers;
+    }
+
+    public void setAllowedToViewUsers(boolean allowedToViewUsers) {
+        this.allowedToViewUsers = allowedToViewUsers;
+    }
+
+    public boolean isAllowedToDeleteKweets() {
+        return allowedToDeleteKweets;
+    }
+
+    public void setAllowedToDeleteKweets(boolean allowedToDeleteKweets) {
+        this.allowedToDeleteKweets = allowedToDeleteKweets;
+    }
+
+    public boolean isAllowedToManageRoles() {
+        return allowedToManageRoles;
+    }
+
+    public void setAllowedToManageRoles(boolean allowedToManageRoles) {
+        this.allowedToManageRoles = allowedToManageRoles;
+    }
+
+    public boolean isAllowedToAssignRoles() {
+        return allowedToAssignRoles;
+    }
+
+    public void setAllowedToAssignRoles(boolean allowedToAssignRoles) {
+        this.allowedToAssignRoles = allowedToAssignRoles;
+    }
+
 }
--- Kwetter/Backend/src/main/java/model/User.java
+++ Kwetter/Backend/src/main/java/model/User.java
@@ -1,14 +1,8 @@
 package model;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author jeroe
+ * @author Jeroen
  */
 public class User {
     
