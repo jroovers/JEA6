80a99e98a3343a0fc7c207f2ddfed823e5861a88
Jeroen Roovers <jroovers93@gmail.com>
2019-04-17 16:22:49 +0200

    angular login page

--- Kwetter/Frontend/src/app/login/login.component.ts
+++ Kwetter/Frontend/src/app/login/login.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  model: any = {};
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login() {
+    if (this.model.username != null && this.model.password != null) {
+      console.log("WHATS UP NERD")
+    }
+    console.log("Enter some credentials nerd")
   }
 
 }
